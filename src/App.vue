52203ea847e5c4ee31b6f62c92e28b94708b91ca
Elodie Dubromel <elodie.dubromel@outlook.fr>
2019-09-10 16:58:40 +0200

    ajout de html dans css dans le template de App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>To Do List</h1>
+
+    <h2>Nom de la Liste</h2>
+
+      <div class="container">
+        <ul>
+          <li>Nom de la tâche</li>
+          <li>Nom de la tâche</li>
+          <li>Nom de la tâche</li>
+          <li>Nom de la tâche</li>
+        </ul>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
-  name: 'app',
+  name: '',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1{
+  border: 1px solid black;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: blue;
+  width: 500px;
+  margin-left: 350px; 
+}
+
+h2{
+  border: 1px dotted blue;
+  color: maroon;
+  text-align: center;
+  width: 300px;
+}
+
+.container{
+  border: 1px dotted blue;
+  color: red;
+  width: 300px;
 }
 </style>
